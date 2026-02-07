c24a2909bc16e2491569af6d2df3f407e471c514
NITISH SHARMA <Nitish.ns378@gmail.com>
2026-02-07 22:27:30 +0000

    studio-5006640178:~/studio{adding-multi-language-feature}$ npm run andro

--- next.config.ts
+++ next.config.ts
@@ -6,26 +6,6 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
   typescript: {
     ignoreBuildErrors: true,
